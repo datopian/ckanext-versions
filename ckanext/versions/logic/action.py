b54ad6cee3731ac7ead475f3c8b51245d95f2311
Shahar Evron <shahar.evron@datopian.com>
2019-08-14 20:53:18 +0300

    Missed commit

--- ckanext/versions/logic/action.py
+++ ckanext/versions/logic/action.py
@@ -1,10 +1,53 @@
 # encoding: utf-8
+import logging
+from datetime import datetime
+
+from ckan import model as core_model
+from ckan.plugins import toolkit
+
+from ckanext.versions.model import DatasetVersion
+
+log = logging.getLogger(__name__)
 
 
 def dataset_version_create(context, data_dict):
-    """Create a new version from the current revision
+    """Create a new version from the current dataset's revision
+
+    Currently you must have editor level access on the dataset
+    to create a version.
+
+    :param dataset: the id or name of the dataset
+    :type dataset: string
+    :param name: A short name for the version
+    :type name: string
+    :param description: A description for the version
+    :type description: string
+    :returns: the newly created version
+    :rtype: dictionary
     """
-    pass
+    model = context.get('model', core_model)
+    dataset_id, name = toolkit.get_or_bust(data_dict, ['dataset', 'name'])
+
+    dataset = model.Package.get(dataset_id)
+    if not dataset:
+        raise toolkit.ObjectNotFound('Dataset not found')
+
+    toolkit.check_access('dataset_version_create', context, data_dict)
+
+    version = DatasetVersion(package_id=dataset.id,
+                             package_revision_id=dataset.revision_id,
+                             name=name,
+                             description=data_dict.get('description', None),
+                             created=datetime.utcnow(),
+                             # TODO: is this right?
+                             creator_user_id=context['user'])
+
+    model.Session.add(version)
+    model.repo.commit()
+
+    log.info('Version "%s" created for package %s', name, dataset.id)
+
+    return version.as_dict()
 
 
 def dataset_version_list(context, data_dict):
