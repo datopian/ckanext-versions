9a7ca4929e88ce098b18f8502ee1d997fe273b2b
Sivan Grunberg <sivang@gmail.com>
2019-08-19 15:54:52 +0300

    fixed and should work api demo script

--- api-demo.sh
+++ api-demo.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -x
 
-API_KEY = "9f6f04ef-2dca-4028-8d6a-7a0019a2484b"
+API_KEY="9f6f04ef-2dca-4028-8d6a-7a0019a2484b"
 
 echo "Create a version for the current revision of my-version-dataset"
-curl -H "Authorization: $API_KEY" -X POST http://ckan-dev:5000/api/3/action/dataset_version_create?dataset=my-version-dataset&name=Release01&description='initial release'
+curl -H "Authorization:$API_KEY" -X POST http://ckan-dev:5000/api/3/action/dataset_version_create?dataset=my-version-dataset&name=Release01&description='initial release'
 
 
 
 echo "List versions for the dataset"
-curl -X GET http://ckan-dev:5000/api/3/action/dataset_version_list?dataset=my-version-dataset
+curl -H "Authorization:$API_KEY" -X GET http://ckan-dev:5000/api/3/action/dataset_version_list?dataset=my-version-dataset
 
-# echo "Now user the revision in the version to get the dataset"
-# curl -X GET http://ckan-dev:5000/api/3/action/package_show_revision?id=my-version-dataset&revision_id=<revision_id>
+# echo "Now show the revision in the version to get the dataset"
+# curl -H "Authorization:$API_KEY" -X GET http://ckan-dev:5000/api/3/action/package_show_revision?id=my-version-dataset&revision_id=<revision_id>
