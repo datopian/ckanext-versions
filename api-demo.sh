9692c5823972001f0f20a4a6d365a7388fe8cbc8
Sivan Grunberg <sivang@gmail.com>
2019-08-19 15:25:28 +0300

    fix usage

--- api-demo.sh
+++ api-demo.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+API_KEY = "9f6f04ef-2dca-4028-8d6a-7a0019a2484b"
 
 echo "Create a version for the current revision of my-version-dataset"
-curl -X GET http://ckan-dev:5000/api/3/action/dataset_version_create?dataset=my-version-dataset&name=Release01&description='initial release'
+curl -H "Authorization: $API_KEY" -X POST http://ckan-dev:5000/api/3/action/dataset_version_create?dataset=my-version-dataset&name=Release01&description='initial release'
 
 
 
