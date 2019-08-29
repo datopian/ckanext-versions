5fd8934db9abb91553a6d847e30b326df7d2e47b
Shahar Evron <shahar.evron@datopian.com>
2019-08-29 13:43:42 +0300

    Allow anon access to read function
    Also properly test authorization to access the dataset (plays more nicely with plugins etc.)

--- ckanext/versions/logic/auth.py
+++ ckanext/versions/logic/auth.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
-from ckan.logic.auth.get import package_show
-from ckan.logic.auth.update import package_update
+from ckan.authz import is_authorized
+from ckan.plugins import toolkit
 
 
 def dataset_version_create(context, data_dict):
@@ -8,7 +8,8 @@ def dataset_version_create(context, data_dict):
 
     This is permitted only to users who are allowed to modify the dataset
     """
-    return package_update(context, {"id": data_dict['dataset']})
+    return is_authorized('package_update', context,
+                         {"id": data_dict['dataset']})
 
 
 def dataset_version_delete(context, data_dict):
@@ -16,20 +17,23 @@ def dataset_version_delete(context, data_dict):
 
     This is permitted only to users who are allowed to modify the dataset
     """
-    return package_update(context, {"id": data_dict['dataset']})
+    return is_authorized('package_update', context,
+                         {"id": data_dict['dataset']})
 
 
+@toolkit.auth_allow_anonymous_access
 def dataset_version_list(context, data_dict):
     """Check if a user is allowed to list dataset versions
 
     This is permitted only to users who can view the dataset
     """
-    return package_show(context, {"id": data_dict['dataset']})
+    return is_authorized('package_show', context, {"id": data_dict['dataset']})
 
 
+@toolkit.auth_allow_anonymous_access
 def dataset_version_show(context, data_dict):
     """Check if a user is allowed to view dataset versions
 
     This is permitted only to users who can view the dataset
     """
-    return package_show(context, {"id": data_dict['dataset']})
+    return is_authorized('package_show', context, {"id": data_dict['dataset']})
