2a97d3450fcfa5fe3dbf111230fdabdb47469901
Sivan Grünberg <sivan.grunberg@datopian.com>
2019-11-13 15:32:13 +0000

    WIP on fix for https://gitlab.com/datopian/ckanext-versions/issues/20
    This change overrides the core resource_show action, which may be a bit risky, but will work if no other extensions try the same :(

--- ckanext/versions/logic/helpers.py
+++ ckanext/versions/logic/helpers.py
@@ -10,6 +10,24 @@ def get_show_url(package_name, version):
                            version=version['id'])
 
 
+def get_resource_show_url(package_name, resource_id, version):
+    """Get the URL for 'package_show' for a version
+    """
+    extra_params = {}
+    if version:
+        dataset_id = "{}@{}".format(package_name,
+                                    version['package_revision_id'])
+        extra_params['version'] = version['id']
+    else:
+        dataset_id = package_name
+
+    return toolkit.url_for(controller='package',
+                           action='resource_read',
+                           id=dataset_id,
+                           resource_id=resource_id,
+                           **extra_params)
+
+
 def has_link_resources(package):
     """Return True if any resource in the dataset is a link to an external
     resource.
