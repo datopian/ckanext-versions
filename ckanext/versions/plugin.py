529938c40dd4e050d55273a69d401957a6492260
Shahar Evron <shahar.evron@datopian.com>
2019-11-27 18:36:18 +0200

    WIP on preview fixing;
    Refactor all url_for wrapping helpers into a single function

--- ckanext/versions/plugin.py
+++ ckanext/versions/plugin.py
@@ -67,9 +67,9 @@ class VersionsPlugin(plugins.SingletonPlugin):
 
     def get_helpers(self):
         return {
-            'dataset_version_show_url': helpers.get_show_url,
-            'dataset_version_resource_show_url': helpers.get_resource_show_url,
-            'dataset_version_get_show_url': helpers.get_show_url,
+            'url_for_version': helpers.url_for_version,
+            # 'dataset_version_show_url': helpers.get_show_url,
+            # 'dataset_version_resource_show_url': helpers.get_resource_show_url,
             'dataset_version_has_link_resources': helpers.has_link_resources,
         }
 
