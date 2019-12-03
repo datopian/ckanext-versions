0a5d0e46fbf23e96e46e92b311bd930e8cbcc249
pdelboca <patriciodelboca@gmail.com>
2019-12-03 11:10:31 +0100

    [#22] Add flash message.

--- ckanext/versions/logic/action.py
+++ ckanext/versions/logic/action.py
@@ -154,6 +154,11 @@ def dataset_version_promote(context, data_dict):
         version.name,
         promoted_dataset['title'])
 
+    toolkit.h.flash_success(
+        toolkit._('Version {} was promoted succesfully.'.format(
+            version.name
+        )))
+
     return promoted_dataset
 
 
