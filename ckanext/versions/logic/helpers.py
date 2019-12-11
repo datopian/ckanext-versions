26f253ee9357609f15ef4dcdaab9d65ed2e9a218
Adrià Mercader <amercadero@gmail.com>
2019-12-11 10:33:33 +0000

    Apply suggestion to ckanext/versions/logic/helpers.py

--- ckanext/versions/logic/helpers.py
+++ ckanext/versions/logic/helpers.py
@@ -63,7 +63,7 @@ def compare_pkg_dicts(old, new, old_activity_id):
     '''
     Takes two package dictionaries that represent consecutive versions of
     the same dataset and returns a list of detailed & formatted summaries of
-    the changes between the two versions. old and new are the two package
+    the changes between the two versions. Old and new are the two package
     dictionaries. The function assumes that both dictionaries will have
     all of the default package dictionary keys, and also checks for fields
     added by extensions and extra fields added by the user in the web
