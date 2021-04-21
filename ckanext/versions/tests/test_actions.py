96e61a218ea9c83cee0f5442410b3a67d76ae269
pdelboca <patriciodelboca@gmail.com>
2021-04-21 14:49:39 -0300

    Fix authentication logic in resource_version_create

--- ckanext/versions/tests/test_actions.py
+++ ckanext/versions/tests/test_actions.py
@@ -32,6 +32,29 @@ class TestCreateResourceVersion(object):
         assert version['name'] == '1'
         assert version['creator_user_id'] == user['id']
 
+    def test_resource_version_create_editor_user(self):
+        user = factories.User()
+        owner_org = factories.Organization(
+            users=[{'name': user['name'], 'capacity': 'editor'}]
+        )
+        dataset = factories.Dataset(owner_org=owner_org['id'])
+        resource = factories.Resource(package_id = dataset['id'])
+
+        version = resource_version_create(
+            get_context(user),{
+                'resource_id': resource['id'],
+                'name': '1',
+                'notes': 'Version notes'
+            }
+        )
+
+        assert version
+        assert version['package_id'] == dataset['id']
+        assert version['resource_id'] == resource['id']
+        assert version['notes'] == 'Version notes'
+        assert version['name'] == '1'
+        assert version['creator_user_id'] == user['id']
+
     def test_cannot_create_versions_with_same_name(self):
         dataset = factories.Dataset()
         resource = factories.Resource(package_id = dataset['id'])
