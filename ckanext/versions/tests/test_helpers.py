24b79f108385952a00d77290f1beb314e91e696e
pdelboca <patriciodelboca@gmail.com>
2021-05-16 12:39:46 -0300

    Fix test

--- ckanext/versions/tests/test_helpers.py
+++ ckanext/versions/tests/test_helpers.py
@@ -2,6 +2,7 @@ import pytest
 
 from ckanext.versions import helpers
 
+@pytest.mark.ckan_config('ckan.site_url', 'http://ckan:5000')
 def test_version_download_url_without_filename():
     url = 'http://ckan:5000/dataset/<id>/resource/<resource_id>/download/'
     download_url = helpers.download_url(url, 'v1.0')
@@ -9,6 +10,7 @@ def test_version_download_url_without_filename():
     assert download_url == 'http://ckan:5000/dataset/<id>/resource/<resource_id>/v/v1.0/download/'
 
 
+@pytest.mark.ckan_config('ckan.site_url', 'http://ckan:5000')
 def test_version_download_url_with_filename():
     url = 'http://ckan:5000/dataset/<id>/resource/<resource_id>/download/filename.csv'
     download_url = helpers.download_url(url, 'v1.0')
@@ -16,6 +18,7 @@ def test_version_download_url_with_filename():
     assert download_url == 'http://ckan:5000/dataset/<id>/resource/<resource_id>/v/v1.0/download/filename.csv'
 
 
+@pytest.mark.ckan_config('ckan.site_url', 'http://ckan:5000')
 def test_version_download_url_with_external_url():
     url = 'https://www.my-external-url.com/external-filename.csv'
     download_url = helpers.download_url(url, 'v1.0')
